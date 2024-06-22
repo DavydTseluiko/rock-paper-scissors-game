490970083fdee67793ffe7eefec355ce137da136
Davyd Tseluiko <tseluiko.davyd@gmail.com>
2024-06-22 16:35:24 -0500

    Write pseudocode for computer logic in the game

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log("Hello World!")
+// Computer logic for the game
+
+// 1. Create a function for computer logic
+// 2. Generate random number from 1 to 3 and store it in the variable
+// 3. Create a variable for computer choice
+// 4. Then if random number equal to  1 - "rock", 2 - "paper", 3 - "scissors" and assign ot the variable of computer choice
+// 5. return the result
