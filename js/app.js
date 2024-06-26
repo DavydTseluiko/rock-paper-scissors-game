8750eb7ad1344449bb9059d565face6bf1c7cadc
Davyd Tseluiko <tseluiko.davyd@gmail.com>
2024-06-26 10:49:02 -0500

    Delete all unnecessary comments and add 3 buttons to HTML

--- js/app.js
+++ js/app.js
@@ -1,18 +1,11 @@
-// * Variables to keep track of the score
 let computerScore = 0;
 let humanScore = 0;
 
-// * Computer logic for the game
-
-// 1. Create a function for computer logic
 function getComputerChoice() {
-    // 2. Generate random number from 1 to 3 and store it in the variable
     let randomNumber = Math.floor(Math.random() * 3) + 1;
 
-    // 3. Declare a variable for computer choice
     let computerChoice;
 
-    // 4. Then if random number equal to  1 - "rock", 2 - "paper", 3 - "scissors" and assign ot the variable of computer choice
     switch (randomNumber) {
         case 1:
             computerChoice = "rock";
@@ -25,17 +18,10 @@ function getComputerChoice() {
             break;
     }
 
-    // 5. Return the result
     return computerChoice;
 }
 
-// * Human choice
-
-// 1. Create a function
 function getHumanChoice() {
-    // 2. Ask user for his choice and store it in the variable and convert it to lowercase
-    // 3. If choice is not "rock", "paper" or "scissors" go to step 2
-    // ! It's possible to combine 2 previous steps in 1 and realize it with do while loop
     let userChoice;
     do {
         userChoice = prompt(
@@ -47,25 +33,16 @@ function getHumanChoice() {
         userChoice !== "scissors"
     );
 
-    // 4. Return the result of the function
     return userChoice;
 }
 
-// * Logic of the round
 
-// 1. Create a function that takes computer and human choices as arguments
 function playRound(computerChoice, humanChoice) {
-    // ! These two variables below created in order to make output capitalized
     let capitalizedComputerOutput =
         computerChoice[0].toUpperCase() + computerChoice.slice(1);
     let capitalizedHumanOutput =
         humanChoice[0].toUpperCase() + humanChoice.slice(1);
 
-    // 2. Write rules of the game and output the result
-    //      rock - beat scissors, and lose to paper
-    //      paper - beat rock, and lose to scissors
-    //      scissors - beat paper, and lose to rock
-    // 3. Increase score of winner
     if (
         (computerChoice === "rock" && humanChoice === "scissors") ||
         (computerChoice === "paper" && humanChoice === "rock") ||
@@ -92,20 +69,13 @@ function playRound(computerChoice, humanChoice) {
     }
 }
 
-// * Write logic to play entire game
-
-// 1. Create a function
 function playGame() {
-    // 2. Call function playRound five times
     for (i = 0; i < 5; i++) {
-        // 3. Ask for human and computer scores
         const humanSelection = getHumanChoice();
         const computerSelection = getComputerChoice();
 
-        // 4. Get the function result and store it in the variable
         let roundFunctionValue = playRound(computerSelection, humanSelection);
 
-        // 5. Keep track of the scores
         if (roundFunctionValue === "Human") {
             humanScore++;
         } else if (roundFunctionValue === "Computer") {
@@ -113,7 +83,6 @@ function playGame() {
         }
     }
 
-    // 6. Print a winner
     if (computerScore > humanScore) {
         console.log(
             `Computer won the game! You had ${humanScore} points while computer had ${computerScore}.`
@@ -127,10 +96,4 @@ function playGame() {
     }
 }
 
-playGame();
 
-// * Code below needed to play only 1 round
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
-
-// playRound(computerSelection, humanSelection);
